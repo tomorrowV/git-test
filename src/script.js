ad51f33bc0df1e75036b3a7f15e1890e8b2c676d
Vladislav Danilov <tomorrow@MacBook-Pro-Vladislav.local>
2023-10-26 10:57:45 +0300

    feature: deposit function in Account Class

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
-class BankAccount {
-    constructor(owner, balance = 0) {
-        this.owner = owner;
-        this.balance = balance;
+class Account {
+    constructor(name) {
+        this.name = name;
+        this.balance = 0;
+    }
+    deposit(amount) {
+        if (amount > 0) {
+            this.balance += amount;
+        }
     }
 }
